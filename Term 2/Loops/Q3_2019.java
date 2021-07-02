08d8825049eb38cb43b119a0fbaebb460e7333cd
Not-Hiesenberg <keerannmoodley@gmail.com>
2021-07-02 10:13:20 +0200

    Create Q3_2019.java

---
+++ Term 2/Loops/Q3_2019.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q3_2019;
+
+/**
+ *
+ * @author keera
+ */
+public class Q3_2019 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x =(int) Math.round(Math.random()*(6-1)+1);
+    }
+    
+}
